b352e562242c32091c1dab623eed93bc01a0d31e
Mohamud Abshir <maxamuudyare21@gmail.com>
2022-05-12 15:44:16 +0300

    added new Section to the hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,52 +5,88 @@ import { motion } from "framer-motion";
 
 const Hero: React.FC = () => {
   return (
-    <div className="mk__hero">
-      <motion.p
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        whileInView={{ opacity: [0, 1], y: [20, 0] }}
-        transition={{ duration: 0.5, type: "spring", stiffness: 12 }}
-        className="subtitle "
-      >
-        Let Ai help your
-      </motion.p>
-      <motion.h1
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        whileInView={{ opacity: [0, 1], y: [-20, 0] }}
-        transition={{ duration: 0.5, type: "spring", stiffness: 12 }}
-        className="heading"
-      >
-        MARKETING TEAM
-      </motion.h1>
-      <motion.p
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        whileInView={{ opacity: [0, 1], y: [20, 0] }}
-        transition={{ duration: 0.5, type: "spring", stiffness: 12 }}
-        className="desc"
-      >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas purus
-        odio tempor rutrum condimentum viverra ornare. Gravida tellus ut
-        venenatis.
-      </motion.p>
-      <motion.button
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        whileInView={{ opacity: [0, 1], scale: [0.9, 1] }}
-        transition={{ duration: 0.5, type: "spring", stiffness: 1 }}
-        className="btn btn-border"
-      >
-        Let's go
-        <img src={images.Arrow} alt="Arrow Image" />
-      </motion.button>
-      <img
-        className="btn scroll-down"
-        src={images.GoDown}
-        alt="Scroll to bottom"
-      />
-    </div>
+    <>
+      <section className="mk__hero">
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          whileInView={{ opacity: [0, 1], y: [20, 0] }}
+          transition={{ duration: 0.5, type: "spring", stiffness: 12 }}
+          className="subtitle "
+        >
+          Let Ai help your
+        </motion.p>
+        <motion.h1
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          whileInView={{ opacity: [0, 1], y: [-20, 0] }}
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            stiffness: 12,
+          }}
+          className="heading"
+        >
+          MARKETING TEAM
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          whileInView={{ opacity: [0, 1], y: [20, 0] }}
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            stiffness: 12,
+          }}
+          className="desc"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+          purus odio tempor rutrum condimentum viverra ornare. Gravida tellus ut
+          venenatis.
+        </motion.p>
+        <motion.button
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          whileInView={{ opacity: [0, 1], scale: [0.9, 1] }}
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            stiffness: 1,
+          }}
+          className="btn btn-border"
+        >
+          Let's go
+          <img src={images.Arrow} alt="Arrow Image" />
+        </motion.button>
+        <img
+          className="btn scroll-down"
+          src={images.GoDown}
+          alt="Scroll to bottom"
+        />
+      </section>
+
+      {/* details  */}
+      <section className="mk__detail">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          whileInView={{ opacity: [0, 1], scale: [0.9, 1] }}
+          transition={{ duration: 0.5, type: "spring", stiffness: 1 }}
+          className="mk__detail--img"
+        >
+          <img src={images.AiRobot} alt="Robot Image" />
+        </motion.div>
+        <div className="mk__detail--text">
+          <motion.p
+            initial={{ opacity: 0, y: -12 }}
+            animate={{ opacity: 1, y: 0 }}
+            whileHover={{ opacity: [0, 1], y: [-40, 0] }}
+          >
+            Make your marketing completely based on Artificial Intelligence.
+          </motion.p>
+        </div>
+      </section>
+    </>
   );
 };
 export default Hero;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,13 @@
 import Logo from "../assets/markaiLogo.png";
 import Arrow from "../assets/Arrow .svg";
 import GoDown from "../assets/go-down.svg";
+import AiRobot from "../assets/headerAi.svg";
 
 const images = {
   Logo,
   Arrow,
   GoDown,
+  AiRobot,
 };
 
 export default images;
