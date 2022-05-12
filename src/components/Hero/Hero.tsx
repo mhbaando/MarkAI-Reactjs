06c15356c199e961efc8637f3557c9e02c9b23d6
Mohamud Abshir <maxamuudyare21@gmail.com>
2022-05-12 14:59:52 +0300

    Implemented Hero Section and it's responsive with well aniamted using framer motion

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
-import '../../styles/hero.scss';
+import React from "react";
+import "../../styles/hero.scss";
+import images from "../../constants";
+import { motion } from "framer-motion";
 
 const Hero: React.FC = () => {
   return (
     <div className="mk__hero">
-      <h1>Hero</h1>
+      <motion.p
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        whileInView={{ opacity: [0, 1], y: [20, 0] }}
+        transition={{ duration: 0.5, type: "spring", stiffness: 12 }}
+        className="subtitle "
+      >
+        Let Ai help your
+      </motion.p>
+      <motion.h1
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        whileInView={{ opacity: [0, 1], y: [-20, 0] }}
+        transition={{ duration: 0.5, type: "spring", stiffness: 12 }}
+        className="heading"
+      >
+        MARKETING TEAM
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        whileInView={{ opacity: [0, 1], y: [20, 0] }}
+        transition={{ duration: 0.5, type: "spring", stiffness: 12 }}
+        className="desc"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas purus
+        odio tempor rutrum condimentum viverra ornare. Gravida tellus ut
+        venenatis.
+      </motion.p>
+      <motion.button
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        whileInView={{ opacity: [0, 1], scale: [0.9, 1] }}
+        transition={{ duration: 0.5, type: "spring", stiffness: 1 }}
+        className="btn btn-border"
+      >
+        Let's go
+        <img src={images.Arrow} alt="Arrow Image" />
+      </motion.button>
+      <img
+        className="btn scroll-down"
+        src={images.GoDown}
+        alt="Scroll to bottom"
+      />
     </div>
   );
 };
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,11 @@
-import Logo from '../assets/markaiLogo.png';
+import Logo from "../assets/markaiLogo.png";
+import Arrow from "../assets/Arrow .svg";
+import GoDown from "../assets/go-down.svg";
 
 const images = {
   Logo,
+  Arrow,
+  GoDown,
 };
 
 export default images;
